8cb45cee30249eeb473709cdfb06b6d30e892eaa
Marrowsed <marcus.ah@gmail.com>
2022-08-02 13:17:16 -0300

    comments

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from .models import *
 
+
 class RevenueSerial(serializers.ModelSerializer):
+    """
+    Revenue Serializer for all
+    """
     class Meta:
         model = Revenue
         fields = "__all__"
 
+
 class ExpenseSerial(serializers.ModelSerializer):
+    """
+    Expense Serializer for all
+    """
     class Meta:
         model = Expense
         fields = "__all__"
