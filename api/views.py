5c2cd9848bad99d39de64010b1cb499f8c5f5e26
Marrowsed <marcus.ah@gmail.com>
2022-08-08 11:52:07 -0300

    implementing filter by description

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from django_filters.rest_framework import DjangoFilterBackend
 
 from api.models import *
 from api.serializers import *
@@ -22,15 +23,23 @@ def api_webhook(request, format=None):
 
 class RevenueViewSet(viewsets.ModelViewSet):
     """
-    Revenue ViewSet
+    Revenue ViewSet with description filter
     """
     queryset = Revenue.objects.all()
     serializer_class = RevenueSerial
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['description']
+
+
 
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     """
-    Expense ViewSet
+    Expense ViewSet with description filter
     """
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerial
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['description']
+
+
